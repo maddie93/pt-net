6b43e9e19f35e71cfc0b38e35ee03abd5ec474ea
wylly <wylly31@wp.pl>
2015-05-26 01:45:47 +0200

    Poprawione znajdowanie aktywnych przejsc

--- js/components/content/models/graph_alghoritms.js
+++ js/components/content/models/graph_alghoritms.js
@@ -3,44 +3,47 @@ module.exports = Backbone.Model.extend({
         this.getActiveTransitions(model);
 
    },
+
    getActiveTransitions: function(model){
        var transitions = model.get('transitions');
        var activeTransactions = [];
        this.setInLinksForAllTransitions(transitions,model);
-       for(var transition in transitions){
-           if(this.isTransitionActive(transition,model)){
-               activeTransactions.push(transition);
+       _.each(transitions,function(entry){
+           if(this.isTransitionActive(entry,model)){
+               activeTransactions.push(entry);
            }
-       }
-
+       },this);
    },
+
     setInLinksForAllTransitions: function(transitions,model){
-        for (var entry in transitions){
+        _.each(transitions,function(entry){
             this.setInLinks(entry,model)
-        }
+        },this);
     },
+
     setInLinks: function(transition, model){
         var inLinks = [];
-        var allLinks = model.getLinks;
-        for(var entry in allLinks){
+        var allLinks = model.getLinks();
+        _.each(allLinks,function(entry){
             if(this.isPointingToTransition(entry,transition)){
                 inLinks.push(entry);
             }
-        }
+        },this);
         transition.inLinks = inLinks;
     },
+
     isPointingToTransition: function(entry,transition){
         return entry.attributes.target.id == transition.id
     },
+
     isTransitionActive: function(transition,model){
         var active = true;
-        for(var link in transition.inLinks){
+        _.each(transition.inLinks,function(link){
             var place = model.getCell(link.attributes.source.id);
             if(place.attributes.tokens<link.getCount){
                 active = false;
-                break;
             }
-        }
+        });
         return active;
     }
 });
